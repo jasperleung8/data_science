6dd1489bd608b020e14acc66051064926454bdeb
jasperleung8 <jasperleung.lego@gmail.com>
2026-03-03 02:55:05 +0800

    Implement data preprocessing in MI_Basics.py
    This script reads data from a CSV file, handles missing values, encodes categorical data, and splits the dataset into training and test sets.

---
+++ MI_Basics.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("Data.csv")
+
+#Spliting x and y
+
+x = data.iloc[:,:-1].values
+y = data.iloc[:,-1].values
+
+print(x)
+print(y)
+
+#handling missing data
+
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(missing_values=np.nan,strategy="mean")
+
+imputer.fit(x[:,1:3])
+x[:,1:3] = imputer.transform(x[:,1:3])
+
+print(x)
+
+#encoding categorical data
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+data2 = ColumnTransformer(transformers=[("encoder",OneHotEncoder(),[0])],remainder="passthrough")
+x = np.array(data2.fit_transform(x))
+print("after conventing words into numbers")
+print(x)
+
+#Conventing yes and no into 1 & 0
+
+from sklearn.preprocessing import LabelEncoder
+
+encoder = LabelEncoder()
+y = encoder.fit_transform(y)
+print("After conventing output labels")
+print(y)
+
+#Spliting into training and test data
+
+from sklearn.model_selection import train_test_split
+
+xTrain,xTest,yTrain,yTest = train_test_split(x,y,test_size=0.3,random_state=1)
+print("Training input")
+print(xTrain)
+print("Testing input")
+print(xTest)
+
+print("Training output")
+print(yTrain)
+print("Testing output")
+print(yTest)
