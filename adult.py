14e58ac3e8d9180a51a7bcbe9b82b31ed4fddf4d
jasperleung8 <jasperleung.lego@gmail.com>
2026-04-20 18:50:43 +0100

    Add data processing script for adult dataset
    This script processes the adult dataset by loading, cleaning, and transforming it. It includes data exploration and prepares the income column for analysis.

---
+++ adult.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
+import seaborn as sns
+import pandas as pd
+
+data = pd.read_csv("adult.csv")
+
+data.columns = ["age","work_class","id","study","study_class","marriage_state","occupation","relateionship","race","gender","gain","lose","work_per_week","country","income"]
+data.rename(columns={"work_class":"work","study_class":"class","marriage_state":"marriage"})
+
+print(data.describe())
+print(data.info())
+
+#Count of missing values
+
+print(data.isnull().sum())
+
+#Finding special character
+
+print(data.isin(["?"]).sum(axis=0))
+
+#Finding unique values
+
+for i in data.columns:
+    print("---- %s ---"% i)
+    print(data[i].value_counts())
+
+#droping columes
+
+data.drop(["id","study_class","work_per_week","gain","lose","country"],axis=1,inplace=True)
+income = set(data["income"])
+print(income)
+
+#maping data into numbers
+
+data["income"]=data["income"].map({'<=50k':0,'>50k':1}).astype(int)
+print(data.head())
